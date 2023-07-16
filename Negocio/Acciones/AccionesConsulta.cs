8af99647998dbd86c9c723bb2289603e59910041
BerryMerry <gabrielggonzalezz@gmail.com>
2023-07-16 14:25:40 -0400

    First commit

---
+++ Negocio/Acciones/AccionesConsulta.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.DBConexion;
+using System.Data;
+using System.Windows.Markup;
+
+namespace Modelo.Acciones 
+{ 
+
+        public class AccionesConsulta : AccionesBD
+        {
+        //Metodo de listado 
+        #region Listado
+        public List<Motele> listMoteles()
+            {
+                return dbLibContext.Moteles.ToList();
+
+            }
+        #endregion
+
+        //Metodo para guardar
+        #region Registrar
+        public string registrarMotel(string nombre, string ubicacion, int idcalificacion, int idsector)
+        {
+            string resultado = "";
+            try
+            {
+                Motele moteles = new Motele
+                {
+                    Nombre = nombre,
+                    Ubicacion = ubicacion,
+                    ID_Sector = idsector,
+                    ID_Calificacion = idcalificacion,
+
+                };
+
+                dbLibContext.Moteles.InsertOnSubmit(moteles);
+                dbLibContext.SubmitChanges();
+
+                return resultado = "Se guardó correctamente";
+            }
+            catch (Exception e)
+            {
+
+                return resultado = "No se pudo guardar correctamente";
+            }
+
+            return resultado;
+
+        }
+
+        #endregion
+
+
+        }
+}
---
+++ Vente_Aqui2/Controllers/RegistrarController.cs
@@ -0,0 +1,28 @@
+﻿using Modelo;
+using Modelo.Acciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vente_Aqui2.Controllers
+{
+    public class RegistrarController : Controller
+    {
+        // GET: Registrar
+        public AccionesConsulta consulta = new AccionesConsulta();
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+            public string RegistrarDatos(string nombre_text = "", string Ubicacion_text = "", int idcalificacion = 0, int idsector = 0)
+            {
+                
+                return consulta.registrarMotel(nombre_text, Ubicacion_text, idcalificacion, idsector);
+            }
+    }
+}
